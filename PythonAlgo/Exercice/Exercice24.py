db7a5a5b16f5f9831b8a11d47fc25e2d0bf04a3b
Louaila <lou591@hotmail.fr>
2025-02-14 15:58:16 +0100

    ajoutExo24

--- PythonAlgo/Exercice/Exercice24.py
+++ PythonAlgo/Exercice/Exercice24.py
@@ -1,52 +1,52 @@
+participants = ["Mario", "Luigi", "Link", "Peach", "Kirby"]
 
-concurrents = ["lou", "hamza", "assia", "sephora"]
+def panne_moteur(participants: list):
+    premier = participants.pop(0)
+    participants.append(premier)
+    return participants
 
+def passe_en_tete(participants: list):
+    #participants.insert(1, participants.pop(0))
 
-def panne_moteur(concurrents):
-    if len(concurrents) > 1:
-        premier = concurrents.pop(0)
-        concurrents.append(premier)
+    temp = participants[0]
+    participants[0] = participants[1]
+    participants[1] = temp
+    return participants
 
+def sauve_honneur(participants: list): 
+    participants[-1], participants[-2] = participants[-2], participants[-1]
+    return participants
 
-def passe_en_tete(concurrents):
-   
-    if len(concurrents) > 1:
-        concurrents[0], concurrents[1] = concurrents[1], concurrents[0]
+def tir_blaster(participants: list):
+    return participants.pop(0)
 
+def retour_inattendu(participants: list, participant_touche):
+    participants.append(participant_touche)
+    return participants
 
-def sauve_honneur(concurrents):
-   
-    if len(concurrents) > 1:
-        concurrents[-1], concurrents[-2] = concurrents[-2], concurrents[-1]
+def affichage_course(participants: list): 
+    affichage = ""
+    for participant in participants:
+        index = participants.index(participant)
+        if index == 0:
+            affichage = affichage + f"1er - {participant}, "
+        else:
+            affichage += f"{index+1}eme - {participant}, "
 
+    print(affichage)
 
-def tir_blaster(concurrents):
+def podium(participants: list): 
+    print(f"""
+        1er {participants[0]}
+2eme {participants[1]}
+                3eme {participants[2]}
+          """)
 
-    if concurrents:
-        concurrents.pop(0)
+affichage_course(panne_moteur(participants))
+affichage_course(passe_en_tete(participants))
+affichage_course(sauve_honneur(participants))
+participant_blasterise = tir_blaster(participants)
+affichage_course(participants)
+affichage_course(retour_inattendu(participants, participant_blasterise))
 
-
-def retour_inattendu(concurrents, module):
-
-    concurrents.append(module)
-
-
-
-
-
-panne_moteur(concurrents)
-print(concurrents)
-
-
-sauve_honneur(concurrents)
-print(concurrents)
-
-
-tir_blaster(concurrents)
-print(concurrents)
-
-
-#ça ne fonctionne pas je ne sais pas pourquoi
-retour_inattendu(concurrents)
-print(concurrents)
-
+podium(participants)
